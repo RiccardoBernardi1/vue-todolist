d5010d7a1e635759dcd2b62dd44d7799350c72fa
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-11-15 16:06:44 +0100

    Crea struttura base html e inserisce la lista di oggetti

--- js/main.js
+++ js/main.js
@@ -4,7 +4,24 @@ const {createApp}=Vue;
 createApp({
     data() {
       return {
-        
+        toDoList:[
+            {
+                text:"fare la spesa",
+                done: false
+            },
+            {
+                text:"Tagliare il prato",
+                done: true
+            },
+            {
+                text:"Pagare l'affitto",
+                done: false
+            },
+            {
+                text:"Portare il cane dal veterinario",
+                done: true
+            },
+        ]
       }
     }
 }).mount('#app');
